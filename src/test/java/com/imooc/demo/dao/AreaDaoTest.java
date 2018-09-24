c55a0c0213f95132b552fffc47ccd920f3ec3717
zj21zhengj <570470242@qq.com>
2018-09-25 00:29:49 +0800

    试试gitignore

--- src/test/java/com/imooc/demo/dao/AreaDaoTest.java
+++ src/test/java/com/imooc/demo/dao/AreaDaoTest.java
@@ -6,9 +6,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
+/*
+@SpringBootTest(classes=AreaDao.class)
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
+@WebAppConfiguration   报错
+ */
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
 public class AreaDaoTest {
